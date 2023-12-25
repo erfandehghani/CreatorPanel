b90723aa333242a00aa0da758806969a6514af83
ErfanDehghani <erfan.dehghani67@yahoo.com>
2023-12-25 20:03:06 +0330

    git ignore changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <>
+      {/* Navigation */}
+        <x-admin.nav/>
+
+        {/* Side Navigation */}
+        <x-admin.side-nav/>
+
+
+        <main class="col-span-3 grid grid-cols-3 gap-4">
+
+            {/* Main content of the page */}
+            
+
+        </main>
+
+        {/* An status modal to show what happened after making a request to server */}
+        {/* <x-admin.inform-modal/> */}
+        
+        </>
   )
 }
