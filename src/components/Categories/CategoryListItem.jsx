4fa6b02606ac34f205d1e33cd7dade63f3df8e86
ErfanDehghani <erfan.dehghani67@yahoo.com>
2024-03-20 18:08:16 +0330

    Categories in category page are now selectable

---
+++ src/components/Categories/CategoryListItem.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+
+const CategoryListItem = ({category, active, clickHandler}) => 
+{
+    // These are the classes that it will be used in this component
+    let defaultClasses = "flex items-center justify-between w-full px-4 py-2 font-medium transition duration-100 cursor-pointer hover:bg-light-primary hover:text-blue-primary rounded-4xl text-light-auxiliary"
+    let activeClasses = "flex items-center justify-between w-full px-4 py-2 font-medium transition duration-100 cursor-pointer bg-light-primary text-blue-primary rounded-4xl";
+
+    // Setting the default class and check if it is an active item, than its state
+    let className = defaultClasses;
+    if (active) 
+    {
+        className = activeClasses;
+    }
+        
+    function handleClick(categoryId) 
+    {
+        clickHandler(categoryId)    
+    }
+
+    return(
+        <div key={category.id} className={className} onClick={() => handleClick(category.id)}>
+            <span>{category.name}</span>
+
+            {/* Here we show how many content is created for this specific category */}
+            <span></span>
+        </div>
+    );
+}
+
+export default CategoryListItem;
--- src/components/Catgories.jsx
+++ src/components/Catgories.jsx
@@ -6,69 +6,125 @@ import TextInput from "./Global/Inputs/TextInput";
 import SelectInput from "./Global/Inputs/SelectInput";
 import TextareaInput from "./Global/Inputs/TextareaInput";
 import SubmitInput from "./Global/Inputs/SubmitInput";
+import { useState } from "react";
+import CategoryListItem from "./Categories/CategoryListItem";
 
+const categories = [
+  {
+    id: 1,
+    name: "لوازم الکترونیکی",
+    metaDescription: "آخرین لوازم الکترونیکی و گجت ها را کشف کنید",
+    description:
+      "از گوشی های هوشمند و لپ تاپ ها گرفته تا لوازم خانگی و دوربین ها، بهترین محصولات الکترونیکی را برای نیازهای خود پیدا کنید.",
+    parentId: null,
+  },
+  {
+    id: 2,
+    name: "پوشاک",
+    metaDescription: "با مجموعه پوشاک ما، استایل داشته باشید",
+    description:
+      "آخرین روندها و سبک های مد را در دسته بندی پوشاک ما کشف کنید که شامل لباس های مردانه، زنانه و کودکان می شود.",
+    parentId: null,
+  },
+  {
+    id: 3,
+    name: "کتاب ها",
+    metaDescription: "در دنیای کتاب ها غرق شوید",
+    description:
+      "کتاب بعدی خود را از میان انتخاب گسترده کتاب ها در ژانرهای مختلف از جمله داستان، غیرداستان و بیشتر پیدا کنید.",
+    parentId: null,
+  },
+  {
+    id: 4,
+    name: "گوشی های هوشمند",
+    metaDescription: "آخرین گوشی های هوشمند را کشف کنید",
+    description:
+      "از مجموعه گوشی های هوشمند جدیدترین و بهترین از برندهای برتر با فناوری پیشرفته و ویژگی های نوآورانه دیدن کنید.",
+    parentId: 1,
+  },
+  {
+    id: 5,
+    name: "پوشاک مردانه",
+    metaDescription: "ضروریات مد مردانه",
+    description:
+      "گزینه های لباس شیک و راحت برای آقایان را کشف کنید، از جمله پیراهن، شلوار، کت و موارد بیشتر.",
+    parentId: 2,
+  },
+];
 
 const Catgories = () => {
+  // Reactive CategoryId
+  const [selectedCategoryId, setCategoryId] = useState(null);
 
+  // Sets the category id and ClassName if user clicked on it
+  function selectCategory(categoryId) {
+    setCategoryId(categoryId);
+  }
 
-    const testOptions = [
-        {id: 1, name: 'Alice', country: 'Austria'},
-        {id: 2, name: 'Bob', country: 'Belgium'},
-        {id: 3, name: 'Carl', country: 'Canada'},
-        {id: 4, name: 'Dean', country: 'Denmark'},
-        {id: 5, name: 'Ethan', country: 'Egypt'},
-      ];
+  return (
+    <>
+      {/* First Section: Choosing a category */}
+      <Card className="col-span-1 h-admin-full-card-height">
+        {/* Card Header */}
+        <CardHeader cardTitle={"دسته بندی ها"} />
 
-	return(
-		<>
-			{/* First Section: Choosing a category */}
-			<Card className="col-span-1 h-admin-full-card-height">
+        {/* Loop over all categories and echo them out */}
+        <div class="flex-grow py-6 space-y-4">
+          {categories.map((category) => {
+            return (
+              <CategoryListItem
+                clickHandler={selectCategory}
+                category={category}
+                active={selectedCategoryId === category.id ? true : false}
+              />
+            );
+          })}
+        </div>
 
-				{/* Card Header */}
-				<CardHeader cardTitle={"دسته بندی ها"}/>
+        {/* Adding new categories */}
+        <div class="flex items-center justify-start font-normal hover:font-bold fill-light-secondary px-4 py-2 hover:fill-secondary duration-100 mt-2 cursor-pointer">
+          <div class="w-7 flex justify-center items-center">
+            <PlusIcon className="size-6 text-inherit" />
+          </div>
+          <span class="mr-4">افزودن دسته جدید</span>
+        </div>
+      </Card>
 
+      {/* Second Section: Editing Or Adding a new category */}
+      <Card className="col-span-2 h-admin-full-card-height">
+        <CardHeader cardTitle={"ویرایش / افزودن دسته"} />
 
-				{/* Loop over all categories and echo them out */}
-				<div class="flex-grow py-6 space-y-4">
-					<div class="flex w-full justify-between items-center px-4 hover:bg-light-primary hover:text-blue-primary py-2 rounded-4xl text-light-auxulary cursor-pointer font-medium transition duration-100">
-						<span>دسته اول</span>
-						<span>55</span>
-					</div>
-				</div>
+        <form class="flex-grow py-8 space-y-6 overflow-y-scroll pl-4">
+          <TextInput
+            name="name"
+            label="نام دسته"
+            description="نام دسته را وارد کنید"
+          />
 
+          <SelectInput
+            name="parent"
+            label="دسته والد - در صورت وجود"
+            description="والد را انتخاب کنید"
+            options={categories}
+          />
 
-				{/* Adding new categories */}
-				<div
-					class="flex items-center justify-start font-normal hover:font-bold fill-light-secondary px-4 py-2 hover:fill-secondary duration-100 mt-2 cursor-pointer">
-					<div class="w-7 flex justify-center items-center">
-						<PlusIcon className="size-6 text-inherit"/>
-					</div>
-					<span class="mr-4">افزودن دسته جدید</span>
-				</div>
-			</Card>
+          <TextareaInput
+            name="metaDesc"
+            label="متا دیسکریپشن دسته"
+            description="باید یک متن شامل 60 الی 160 کارکتر برای نمایش به عنوان شرح صفحه در موتور های جستجو وارد کنید."
+          />
 
-			{/* Second Section: Editing Or Adding a new category */}
-			<Card className="col-span-2 h-admin-full-card-height">
-				<CardHeader cardTitle={"ویرایش / افزودن دسته"}/>
+          <TextareaInput
+            name="description"
+            label="توضیحات صفحه دسته"
+            description="باید یک یا دو پاراگراف حداکثر دارای 480 کارکتر برای نمایش در صفحه مخصوص دسته که مطالب آن نمایش داده میشوند وارد کنید"
+          />
 
-				<form class="flex-grow py-8 space-y-6 overflow-y-scroll pl-4">
-
-					<TextInput name="name" label="نام دسته" description="نام دسته را وارد کنید"/>
-					
-					<SelectInput name="parent" label="دسته والد - در صورت وجود" description="والد را انتخاب کنید" options={testOptions}/>
-
-					<TextareaInput name="metaDesc" label="متا دیسکریپشن دسته"
-					description="باید یک متن شامل 60 الی 160 کارکتر برای نمایش به عنوان شرح صفحه در موتور های جستجو وارد کنید."/>
-					
-					<TextareaInput name="description" label="توضیحات صفحه دسته"
-					description="باید یک یا دو پاراگراف حداکثر دارای 480 کارکتر برای نمایش در صفحه مخصوص دسته که مطالب آن نمایش داده میشوند وارد کنید"/>
-
-					<SubmitInput/>
-				</form>
-
-			</Card>
-		</>
-	);
+          <SubmitInput />
+        </form>
+      </Card>
+    </>
+  );
 };
 
 export default Catgories;
